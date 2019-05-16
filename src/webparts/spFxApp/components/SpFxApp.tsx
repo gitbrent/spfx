69c416fe3968bcf7c9aa52bd7f933d4b60c2a99b
Brent Ely <gitbrent@gmail.com>
2019-05-15 22:28:56 -0500

    WIP

--- src/webparts/spFxApp/components/SpFxApp.tsx
+++ src/webparts/spFxApp/components/SpFxApp.tsx
@@ -1,7 +1,12 @@
+/**
+* ORIG: https://github.com/SharePoint/sp-dev-fx-controls-react/tree/master/src/controls/listItemPicker
+* ADDED: Ability to filter List query using `listFilter` prop
+*/
 import * as React from "react";
 import styles from "./SpFxApp.module.scss";
 import { ISpFxAppProps } from "./ISpFxAppProps";
 import { escape } from "@microsoft/sp-lodash-subset";
+import { ListItemPicker } from '../components/listItemPicker/ListItemPicker';
 
 export default class SpFxApp extends React.Component<ISpFxAppProps, {}> {
   public render(): React.ReactElement<ISpFxAppProps> {
@@ -23,6 +28,17 @@ export default class SpFxApp extends React.Component<ISpFxAppProps, {}> {
             </div>
           </div>
         </div>
+
+		<ListItemPicker
+			listId='12a01bb8-1234-ABCD-b4e9-00dde9c8ef90'
+			columnInternalName='Title'
+			listFilter='Archived_x003f_ eq 1'
+			itemLimit={1}
+			onSelectedItem={this.onSelectedItem}
+			context={this.props.context}
+			suggestionsHeaderText='Select Account Name'
+		/>
+
       </div>
     );
   }
